a21b0d265a885cf911df3fbb69fe6af55eb682fc
Larry <larryle7680@gmail.com>
2026-05-20 15:15:53 -0500

    Prompt the user to search for a name using a for Loop

--- src/main/java/com/pluralsight/Person.java
+++ src/main/java/com/pluralsight/Person.java
@@ -12,4 +12,37 @@ public class Person {
         this.lastName = lastName;
         this.age = age;
     }
+
+    //Getters/Setters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person: \n" +
+                "First Name:" + firstName + "\n" +
+                "Last Name: " + lastName  + "\n" +
+                "Age: " + age ;
+    }
 }
--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -2,25 +2,35 @@ package com.pluralsight;
 
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.*;
 public class Program {
-
+static Scanner theScanner = new Scanner(System.in);
     public static void main(String[] args) {
 
         //Create an empty List
-        List<Person> person = new ArrayList<>();
+        List<Person> personList = new ArrayList<>();
 
         //add 10 people to the list
-        person.add(new Person("Oliver", "Bennett", 34));
-        person.add(new Person("Spencer", "Howell", 44));
-        person.add(new Person("Faith", "Townsend", 22));
-        person.add(new Person("Alonso", "Rhodes", 60));
-        person.add(new Person("Virgil", "Bush", 19));
-        person.add(new Person("Riley", "Underwood", 52));
-        person.add(new Person("Ester", "Kramer", 12));
-        person.add(new Person("Madeleine", "Valencia", 76));
-        person.add(new Person("Lenora", "Hurst", 42));
-        person.add(new Person("Morton", "Mcmahon", 80));
+        personList.add(new Person("Oliver", "Bennett", 34));
+        personList.add(new Person("Spencer", "Howell", 44));
+        personList.add(new Person("Faith", "Townsend", 22));
+        personList.add(new Person("Alonso", "Rhodes", 60));
+        personList.add(new Person("Virgil", "Bush", 19));
+        personList.add(new Person("Riley", "Underwood", 52));
+        personList.add(new Person("Ester", "Kramer", 12));
+        personList.add(new Person("Madeleine", "Valencia", 76));
+        personList.add(new Person("Lenora", "Hurst", 42));
+        personList.add(new Person("Morton", "Mcmahon", 80));
+
+        //Prompt the user to find a name
+        System.out.println("Please enter a first name to search: ");
+        System.out.println();
+        String userSearch = theScanner.nextLine();
+        
+        for(Person name : personList){
+            if(name.getFirstName().equalsIgnoreCase(userSearch))
+                System.out.println(name);
+        }
 
     }
 
